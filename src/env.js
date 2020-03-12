c9f6d58c805cb79b6d03191a7f614863153117e1
Chunlai <chunlai0928@foxmail.com>
2020-03-12 21:11:24 +0800

    接口环境设置

--- src/env.js
+++ src/env.js
@@ -1,11 +1,15 @@
 let baseURL;
+// 如需自定义需要创建(.env.自定义名)的文件，里面设置NODE_ENV='自定义名'
 switch (process.env.NODE_ENV) {
+    // 开发环境 npm run development
     case 'development':
         baseURL = 'http://dev-mall-pre.springboot.cn/api';
         break;
+    // 测试环境
     case 'test':
         baseURL = 'http://test-mall-pre.springboot.cn/api';
-        break;   
+        break; 
+    //线上环境
      case 'prod':
         baseURL = 'http://mall-pre.springboot.cn/api';
         break;
