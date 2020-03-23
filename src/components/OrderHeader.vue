9a3ddaa7528a0049530685ab299b6f419a2b71d9
Chunlai <chunlai0928@foxmail.com>
2020-03-23 21:33:05 +0800

    购物车列表渲染

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -11,17 +11,21 @@
         </h2>
       </div>
       <div class="username">
-        <a href="javascript:;">Jack</a>
+        <a href="javascript:;">{{username}}</a>
       </div>
     </div>
   </div>
 </template>
 <script>
+import {mapState} from 'vuex';
 export default {
   name: "order-header",
   props: {
     title: String
-  }
+  },
+  computed: {
+    ...mapState(["username"])
+  },
 };
 </script>
 <style lang="scss">
