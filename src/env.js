d637eb94c7c6866a51af10eaf41dfa7b333b7874
Chunlai <chunlai0928@foxmail.com>
2020-04-07 13:34:45 +0800

    支付宝支付对接

--- src/env.js
+++ src/env.js
@@ -1,11 +1,12 @@
 let baseURL;
 // 如需自定义需要创建(.env.自定义名)的文件，里面设置NODE_ENV='自定义名'
 switch (process.env.NODE_ENV) {
+    // 默认 npm run serve
     // 开发环境 npm run development
     case 'development':
         baseURL = 'http://dev-mall-pre.springboot.cn/api';
         break;
-    // 测试环境
+    // 测试环境 npm run test
     case 'test':
         baseURL = 'http://test-mall-pre.springboot.cn/api';
         break; 
--- src/views/order.vue
+++ src/views/order.vue
@@ -30,14 +30,17 @@ export default {
   mounted() {
     let path = this.$route.path;
     if (path == "/order/confirm") {
-      this.title = '订单确认';
-      this.tip = '请认真填写收货地址';
-    }else if(path == '/order/list'){
-      this.title = '订单列表';
-      this.tip = '谨防钓鱼链接或诈骗电话，了解更多>';
-    }else if(path == '/order/pay'){
-      this.title = '订单支付';
-      this.tip = '谨防钓鱼链接或诈骗电话，了解更多>';
+      this.title = "订单确认";
+      this.tip = "请认真填写收货地址";
+    } else if (path == "/order/list") {
+      this.title = "订单列表";
+      this.tip = "请谨防钓鱼链接或诈骗电话，了解更多>";
+    } else if (path == "/order/pay") {
+      this.title = "订单支付";
+      this.tip = "请谨防钓鱼链接或诈骗电话，了解更多>";
+    } else {
+      this.title = "订单支付";
+      this.tip = "请谨防钓鱼链接或诈骗电话，了解更多>";
     }
   }
 };
